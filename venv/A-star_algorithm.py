871a134f8bc9ffdc52a932c9c154da60d8af5f77
sh-shrey <shreyashahi202@gmail.com>
2021-05-16 17:26:44 +0530

    Initial commit

---
+++ venv/A-star_algorithm.py
@@ -0,0 +1,54 @@
+tree = {'S': [['A', 1], ['B', 2]],
+         'A': [['S', 1]],
+         'B': [['S', 2], ['C', 3], ['D', 4]],
+         'C': [['B', 2], ['E', 5], ['F', 6]],
+         'D': [['B', 4], ['G', 7]],
+         'E': [['C', 5]],
+         'F': [['C', 6]]
+         }
+heuristic = {'S': 0, 'A': 50, 'B': 2, 'C': 3, 'D': 4, 'E': 50, 'F': 50, 'G': 0}
+# total cost for nodes visited
+cost = {'S': 0}
+def AStarSearch():
+    global tree, heuristic
+    closed = []
+    opened = [['S', 8]]
+# find the visited nodes
+    while True:
+        fn = [i[1] for i in opened]     # f(n) = g(n) + h(n)
+        chosen_index = fn.index(min(fn))
+        node = opened[chosen_index][0]  # current node
+        closed.append(opened[chosen_index])
+        del opened[chosen_index]
+        if closed[-1][0] == 'G':        # break the loop if node G has been found
+            break
+        for item in tree[node]:
+            if item[0] in [closed_item[0] for closed_item in closed]:
+                continue
+            cost.update({item[0]: cost[node] + item[1]})            # add nodes to cost dictionary
+            fn_node = cost[node] + heuristic[item[0]] + item[1]     # calculate f(n) of current node
+            temp = [item[0], fn_node]
+            opened.append(temp)                                     # store f(n) of current node in array opened
+
+    '''find optimal sequence'''
+    trace_node = 'E'
+    optimal_sequence = ['E']
+    for i in range(len(closed)-2, -1, -1):
+        check_node = closed[i][0]           # current node
+        if trace_node in [children[0] for children in tree[check_node]]:
+            children_costs = [temp[1] for temp in tree[check_node]]
+            children_nodes = [temp[0] for temp in tree[check_node]]
+
+            if cost[check_node] + children_costs[children_nodes.index(trace_node)] == cost[trace_node]:
+                optimal_sequence.append(check_node)
+                trace_node = check_node
+    # reverse the optimal sequence
+    optimal_sequence.reverse()
+
+    return closed, optimal_sequence
+
+
+if __name__ == '__main__':
+    visited_nodes, optimal_nodes = AStarSearch()
+    print('visited nodes: ' + str(visited_nodes))
+    print('Shortest path: ' + str(optimal_nodes))
---
+++ venv/Canny_edge.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+#from matplotlib import pyplot as plt
+
+img = cv2.imread("building.jpg",0)
+print(img)
